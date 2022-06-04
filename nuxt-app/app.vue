02f996631a65960c1b14bab633139e9dd96e8fe0
kouichi.hoshi <kouichi.hoshi@gmail.com>
2022-06-04 09:51:56 +0900

    sitehHeader追加

--- nuxt-app/app.vue
+++ nuxt-app/app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <siteHeader />
   </div>
 </template>
---
+++ nuxt-app/components/siteHeader.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <header>
+    <h1>header</h1>
+    <nav>
+      <ul>
+        <li>menu1</li>
+        <li>menu2</li>
+      </ul>
+    </nav>
+  </header>
+</template>
