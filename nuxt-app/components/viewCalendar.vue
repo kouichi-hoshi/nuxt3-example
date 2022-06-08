ce024b30c2bd41cc756e2d614c12f0c0d3c42a13
kouichi.hoshi <kouichi.hoshi@gmail.com>
2022-06-08 09:29:47 +0900

    不要コード削除

--- nuxt-app/components/viewCalendar.vue
+++ nuxt-app/components/viewCalendar.vue
@@ -33,7 +33,6 @@ const setDay = () => {
   if (props.setDay) {
     return dayjs(props.setDay)
   }  else {
-    //TODO select要素などから値を渡せるようにする
     return dayjs()
   }
 }
@@ -87,86 +86,6 @@ function nextDays(d:Number) {
   }
 }
 
-
-
-//////////////////////////////////////////
-
-
-//＊ 別実装試行（2022-05の表示にバグあり）
-
-
-interface ThisMonthData2 {
-  data01: [] // 先月の日付の終わり 数字配列
-  data02: Number // 今月の日付の始まり　数字配列
-  data03: Number // 今月の日付の中　数字
-  data04: [] //今月の日付の終わり　数字配列
-  data05: Number //来月の日付の始まり　数字配列
-  data06: [] //来月の日付の始まり　数字配列
-}
-
-// const thisMonthData2:ThisMonthData2 = {
-//   data01: [], // 空白セルを先月の日にちで埋める
-//   data02: [], // 今月の初週の日数
-//   data03: 0, // 今月の日付の中
-//   data04: [], //今月の日付の終わり
-//   data05: [], //空白セルを来月の日にちで埋める
-// }
-const thisMonthData2 = {
-  data01: [],
-  data02: 0,
-  data03: 0,
-  data04: [],
-  data05: 0,
-  data06: 0,
-  data07: [],
-  data08: [],
-}
-
-
-// 先月の日にち
-for (let date:Number = nowDate.startWeekday; date > 0; date--) {
-  thisMonthData2.data01.push(Number(nowDate.lastEndDate) - Number(date) + 1)
-}
-
-//先月の日にちの個数
-thisMonthData2.data02 = thisMonthData2.data01.length
-
-// 今月の日にち
-for (let date:Number = 1; date <=Number(nowDate.endDate); date++) {
-  const current = now.set('date', Number(date))
-  const weekday = current.get('day')
-  if (date > weekday) {
-    thisMonthData2.data07.push(date)
-  } else {
-    thisMonthData2.data08.push(date)
-  }
-}
-
-//当月の日にちの個数
-for (let date:Number = 1; date <=Number(nowDate.endDate); date++) {
-  thisMonthData2.data03 = date
-}
-
-// 来月の日にち
-for (let date:Number = 1; date < (7 - Number(nowDate.endWeekday)); date++) {
-  thisMonthData2.data04[date - 1] = date
-}
-
-//来月の日にちの個数
-thisMonthData2.data05 = thisMonthData2.data04.length
-
-
-// カレンダーに掲載する総個数
-thisMonthData2.data06 = thisMonthData2.data02 + thisMonthData2.data03 + thisMonthData2.data05
-
-// //カレンダー総個数を週に分ける
-let thisWeeks: number[] = []
-for (let i:number = 1; i < thisMonthData2.data06; i++) {
-  if( (i % 7) == 0 ){
-    thisWeeks.push(i)
-  }
-}
-
 </script>
 
 <template>
@@ -207,56 +126,6 @@ for (let i:number = 1; i < thisMonthData2.data06; i++) {
       </tr>
     </tbody>
   </table>
-
-  <div class="m-2">
-    <table>
-      <caption>{{nowDate.today}}-2</caption>
-      <tbody>
-        <tr>
-          <th v-for="(weekday, i) in weekdays" :key="i">{{weekday}}</th>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data01" :key="i">
-            <td class="text-slate-400 pointer-events-none">{{day}}</td>
-          </template>
-          <template v-for="(day, i) in thisMonthData2.data08" :key="i">
-            <td>{{day}}</td>
-          </template>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data07" :key="i">
-            <td v-if="7 > i">{{day}}</td>
-          </template>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data07" :key="i">
-            <td v-if="7 <= i && i < 14">{{day}}</td>
-          </template>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data07" :key="i">
-            <td v-if="14 <= i && i < 21">{{day}}</td>
-          </template>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data07" :key="i">
-            <td v-if="21 <= i && i < 28">{{day}}</td>
-          </template>
-          <template v-for="(day, i) in thisMonthData2.data04" :key="i">
-            <td v-if="thisMonthData2.data06 <= 35" class="text-slate-400 pointer-events-none">{{day}}</td>
-          </template>
-        </tr>
-        <tr>
-          <template v-for="(day, i) in thisMonthData2.data07" :key="i">
-            <td v-if="28 <= i && i < 35">{{day}}</td>
-          </template>
-          <template v-for="(day, i) in thisMonthData2.data04" :key="i">
-            <td v-if="thisMonthData2.data06 > 35" class="text-slate-400 pointer-events-none">{{day}}</td>
-          </template>
-        </tr>
-      </tbody>
-    </table>
-  </div>
 
 </template>
 
